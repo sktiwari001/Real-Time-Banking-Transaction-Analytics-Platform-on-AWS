3abf70167527c18f197873176f604ad52374f344
sktiwari001 <115336112+sktiwari001@users.noreply.github.com>
2026-02-02 13:49:00 +0530

    Update etl_job.py

--- code/glue/etl_job.py
+++ code/glue/etl_job.py
@@ -1,1 +1,64 @@
+"""
 
+AWS Glue Visual ETL Job
+
+Source  : S3 (raw transactions from Kinesis)
+
+Target  : S3 (processed parquet data)
+
+Purpose : Clean, transform, and prepare data for Athena & Redshift
+
+"""
+
+import sys
+
+from pyspark.context import SparkContext
+
+from awsglue.context import GlueContext
+
+from awsglue.job import Job
+
+from awsglue.utils import getResolvedOptions
+
+from pyspark.sql.functions import col, when, current_timestamp
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+
+sc = SparkContext()
+
+glueContext = GlueContext(sc)
+
+spark = glueContext.spark_session
+
+job = Job(glueContext)
+
+job.init(args['JOB_NAME'], args)
+
+# Visual ETL equivalent: S3 Source
+
+raw_df = spark.read.json("s3://<raw-bucket>/transactions/")
+
+# Visual ETL Transform nodes
+
+transformed_df = (
+
+    raw_df
+
+    .withColumn("amount", col("amount").cast("double"))
+
+    .withColumn("fraud_flag", when(col("amount") > 100000, 1).otherwise(0))
+
+    .withColumn("processed_at", current_timestamp())
+
+)
+
+# Visual ETL target: S3 Sink (Parquet)
+
+transformed_df.write \
+
+    .mode("overwrite") \
+
+    .parquet("s3://<processed-bucket>/processed/")
+
+job.commit()
+ 
