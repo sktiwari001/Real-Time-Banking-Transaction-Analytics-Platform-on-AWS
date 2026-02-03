605b60f4527910d7b4c528b06d107dd092e67f22
sktiwari001 <115336112+sktiwari001@users.noreply.github.com>
2026-02-03 10:58:02 +0530

    Update queries.sql

--- code/athena/queries.sql
+++ code/athena/queries.sql
@@ -1,1 +1,22 @@
+1.SELECT *
+FROM banking_raw_ext.transactions_gold
+LIMIT 5;
 
+
+2.SELECT *
+FROM awsdatabasecatalog.banking_analytics_db.transactions_gold
+LIMIT 5;
+
+3.INSERT INTO banking_transactions
+SELECT
+ txn_id,
+ account_id,
+ customer_name,
+ CAST(amount AS INT),
+ channel,
+ branch,
+ CAST(txn_time AS TIMESTAMP),
+ is_suspicious
+FROM
+ awsdatacatalog.banking_analytics_new.transactions_gold;
+ 
